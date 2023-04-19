d5c683c14d2b86fd5948399366685df6dd88faf7
Edwin E <edwin.emilien92@gmail.com>
2023-04-19 11:16:17 -0400

    React testing Project

---
+++ src/App.js
@@ -0,0 +1,65 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+const Content = (props) => {
+
+
+    return (
+      <div >
+        <Part part={props.part} exercise={props.exercises} />
+      </div>
+    )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part} {props.exercise}
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content part={course.parts[0].name} exercises={course.parts[0].exercises}/>
+      <Content part={course.parts[1].name} exercises={course.parts[1].exercises}/>
+      <Content part={course.parts[2].name} exercises={course.parts[2].exercises}/>
+      <Total exercises1={course.parts[0].exercises} exercises2={course.parts[0].exercises} exercises3={course.parts[0].exercises} />
+    </div>
+  )
+}
+
+export default App
